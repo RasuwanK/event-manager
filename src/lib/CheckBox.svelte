fb26b7bcc4667aa65045052d8afb6a745e459c33
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-06 13:05:58 +0530

    styled input fields

---
+++ src/lib/CheckBox.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let label: string = '';
+	export let isChecked: boolean = false;
+	export let name: string;
+</script>
+
+<div id="input-field-checkbox" class="w-full flex flex-row items-center">
+	<label for={name} class="text-sm select-none text-center">{label} </label>
+	<input type="checkbox" bind:checked={isChecked} id={name} {name} />
+</div>
--- src/lib/InputField.svelte
+++ src/lib/InputField.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
-
-  // All the props related to the component
+	// All the props related to the component
 	export let type: 'text' | 'number' | 'date' | 'textarea' | 'email';
 	export let value: string = '';
 	export let name: string;
 	export let isRequired: boolean = false;
-  export let label: string;
+	export let label: string;
 
+	const textboxStyles =
+		'w-full text-[#222222] bg-[#F5F5F5] border border-[#A6A6A6] px-2 py-2 rounded-md text-md resize-none scroll-y';
 </script>
 
-<div class="input-field flex flex-col">
-	<label for={name}>{label}</label>
+<div class="input-field flex flex-col items-stretch gap-3">
+	<label for={name} class="text-sm w-full select-none">{label}</label>
 	{#if type === 'textarea'}
-		<textarea {name} bind:value required={isRequired} />
+		<textarea {name} rows="8" class={textboxStyles} bind:value required={isRequired} />
 	{:else if type === 'text'}
-		<input id={name} {name} bind:value type="text" required={isRequired} />
+		<input id={name} class={textboxStyles} {name} bind:value type="text" required={isRequired} />
 	{:else if type === 'number'}
-		<input id={name} {name} bind:value type="number" required={isRequired} />
+		<input
+			id={name}
+			class={textboxStyles}
+			{name}
+			min="1"
+			bind:value
+			type="number"
+			required={isRequired}
+		/>
 	{:else if type === 'date'}
-		<input id={name} {name} bind:value type="date" required={isRequired} />
+		<input id={name} class={textboxStyles} {name} bind:value type="date" required={isRequired} />
 	{:else}
-		<input id={name} {name} bind:value type="text" required={isRequired} />
+		<input id={name} class={textboxStyles} {name} bind:value type="text" required={isRequired} />
 	{/if}
 </div>
