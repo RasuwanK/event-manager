9ef7f12f8e3d030665e2173d6a718dad60fde299
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-07 19:29:55 +0530

    Added new Button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let type: "submit" | "reset" | "button" = "button";
+</script>
+<button type={type} class="bg-[#222222] text-white p-2 rounded-md">
+  <slot />
+</button>
