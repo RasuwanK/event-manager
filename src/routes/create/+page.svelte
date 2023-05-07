edc6dedfc4f00b5a18aac88d05292a2adf6798c2
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-07 19:30:41 +0530

    Form validation added

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
+	import type { ActionData } from './$types';
+  import {enhance} from '$app/forms';
+
+	export let form: ActionData;
 	import InputField from '$lib/InputField.svelte';
 	import FormCell from '$lib/FormCell.svelte';
-  import CheckBox from '$lib/CheckBox.svelte';
-	// Form field values
-	let name = '';
-	let participantLimit = '1';
-	let venue = '';
-	let date = '';
-	let contactEmail = '';
-	let organizer = '';
-	let description = '';
-  let checked = false;
+	import CheckBox from '$lib/CheckBox.svelte';
+	import Button from '$lib/Button.svelte';
+
 </script>
 
 <div id="create-page" class="mt-4 p-2 flex flex-col items-center">
@@ -18,66 +15,66 @@
 		<header>
 			<h1 class="text-4xl text-center font-bold text-[#222222]">Create your event here</h1>
 		</header>
-		<form method="post" class="p-3 w-full grid grid-cols-2 gap-x-2 gap-y-6">
+		{#if form?.success}
+			<p>Successfully submitted</p>
+		{/if}
+    {#if form?.name.missing}
+      <p>Event name missing</p>
+    {/if}
+		<form method="post" class="p-3 w-full grid grid-cols-2 justify-items-stretch gap-x-2 gap-y-6" use:enhance>
 			<FormCell>
 				<InputField
 					label="Name of the event"
 					type="text"
-					bind:value={name}
 					name="name"
-					isRequired={true}
 				/>
 			</FormCell>
 			<FormCell>
 				<InputField
 					label="Participant Limit"
 					type="number"
-					bind:value={participantLimit}
 					name="participantLimit"
-					isRequired={true}
 				/>
 			</FormCell>
 			<FormCell>
-				<InputField label="Venue" type="text" bind:value={venue} name="venue" isRequired={true} />
+				<InputField label="Venue" type="text" name="venue" isRequired={true} />
 			</FormCell>
 			<FormCell>
 				<InputField
 					label="Date of the event"
 					type="date"
-					bind:value={date}
 					name="date"
-					isRequired={true}
 				/>
 			</FormCell>
 			<FormCell>
 				<InputField
 					label="Contact email"
 					type="email"
-					bind:value={contactEmail}
 					name="contactEmail"
-					isRequired={true}
 				/>
 			</FormCell>
 			<FormCell>
 				<InputField
 					label="Organizer"
 					type="text"
-					bind:value={organizer}
 					name="organizer"
-					isRequired={true}
 				/>
 			</FormCell>
 			<FormCell fullSpan={true}>
 				<InputField
 					label="Description"
 					type="textarea"
-					bind:value={description}
 					name="description"
-					isRequired={true}
 				/>
 			</FormCell>
-			<FormCell fullSpan={true}>
-        <CheckBox name="agree" bind:isChecked={checked} label="I have read and accept all the content included in the terms and conditions." />
+			<FormCell fullSpan={true} isCentered={true}>
+				<CheckBox
+					name="agree"
+					label="I have read and accept all the content included in the terms and conditions."
+				/>
+			</FormCell>
+			<FormCell fullSpan={true} isCentered={true}>
+				<Button type="submit">Create Event</Button>
 			</FormCell>
 		</form>
 	</div>
