5c32a3d02cd3ec345b5eaefb633fb0e9f4d6bbcf
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-07 19:29:09 +0530

    Fixing form cell errors

--- src/lib/FormCell.svelte
+++ src/lib/FormCell.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
-  export let fullSpan: boolean = false;
+	type Utility = {
+		className: string;
+		logic: boolean;
+	};
+
+	export let fullSpan: boolean = false;
+	export let isCentered: boolean = false;
+
+	function addUtility(utility: string, condition: boolean) {
+		if (condition) {
+			return utility;
+		}
+	}
+
+	function addUtilities(utilities: Utility[]) {
+		let classList = '';
+    const space = ' ';
+		utilities.forEach(({ className, logic }) => {
+			if(logic) classList += (className) + space;
+		});
+
+    return classList;
+	}
+
+const computedStyles = addUtilities([
+  {className: 'w-full', logic: !isCentered},
+  {className: 'col-span-full', logic: fullSpan},
+  {className: 'justify-self-center', logic: isCentered}
+]);
 </script>
 
-<div class={`cell w-full ${fullSpan ? 'col-span-full' : ''}`}>
+<div
+	class={`cell ${computedStyles} `}
+>
 	<slot />
 </div>
