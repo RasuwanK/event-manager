d36d54db66014f4d6f3dfa7ed0390fa4617fdd96
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-10 09:11:14 +0530

    Input field is now correcty rendering errors

--- src/lib/InputField.svelte
+++ src/lib/InputField.svelte
@@ -5,19 +5,25 @@
 	export let name: string;
 	export let isRequired: boolean = false;
 	export let label: string;
-  export let error: string = '';
 
 	const textboxStyles =
 		'w-full outline-[#222222] text-[#222222] bg-[#F5F5F5] border border-[#A6A6A6] px-2 py-2 rounded-md text-md resize-none scroll-y';
 </script>
 
 <div class="input-field flex flex-col items-stretch gap-3">
-  <span id="{name}-error" class="text-red">{error}</span>
-	<label for={name} class="text-sm w-full select-none">{label}</label>
+	<label for={name} class="text-sm w-full select-none">
+		<span
+			>{label}<span>
+				{#if isRequired}
+					<span class="text-red-800">*</span>
+				{/if}
+			</span></span
+		></label
+	>
 	{#if type === 'textarea'}
-		<textarea id={name} rows="8" class={textboxStyles} bind:value required={isRequired} />
+		<textarea id={name} {name} rows="8" class={textboxStyles} bind:value required={isRequired} />
 	{:else if type === 'text'}
-		<input id={name} class={textboxStyles} name={name} bind:value type="text" required={isRequired} />
+		<input id={name} class={textboxStyles} {name} bind:value type="text" required={isRequired} />
 	{:else if type === 'number'}
 		<input
 			id={name}
@@ -29,8 +35,8 @@
 			required={isRequired}
 		/>
 	{:else if type === 'date'}
-		<input id={name} class={textboxStyles} name={name} bind:value type="date" required={isRequired} />
+		<input id={name} class={textboxStyles} {name} bind:value type="date" required={isRequired} />
 	{:else}
-		<input id={name} class={textboxStyles} name={name} bind:value type="text" required={isRequired} />
+		<input id={name} class={textboxStyles} {name} bind:value type="text" required={isRequired} />
 	{/if}
 </div>
