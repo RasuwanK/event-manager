b47668b3b05db5d2364ac7ca370d909a2d1ce20f
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-10 09:07:27 +0530

    Error component for form validation

---
+++ src/lib/Error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let name: string;
+  export let invalidMessage: string;
+  export let emptyMessage: string;
+  export let empty: boolean;
+  export let invalid: boolean;
+</script>
+<span class="{name}-error text-red-800 text-sm">
+  {#if empty}
+    {emptyMessage}
+  {:else if invalid}
+    {invalidMessage}
+  {/if}
+</span>
