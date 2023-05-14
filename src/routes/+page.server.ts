84fe1242fa59d0c3ce9a5a15a97a30271d211bd9
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-14 21:57:10 +0530

    Removed unnecessary error handling

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,87 +3,103 @@ import { fail } from '@sveltejs/kit';
 import prisma from '$lib/server/prisma';
 
 export const actions = {
-  default: async ({ request }) => {
-    const submitData = await request.formData();
-    const filter = submitData.get('filter');
-    const query = submitData.get('query');
-    let result: unknown;
-    console.log(query);
-    if (query) {
-      // When user provides a query and a filter
-      try {
-        switch (filter) {
-          case 'bydate':
-            result =
-              await prisma.$queryRaw`SELECT name, participant_limit, bookings, venue, date FROM Event WHERE date = '${query}';`;
-            break;
-          case 'byname':
-            console.log('By name');
-            try {
-              result = await prisma.event.findMany({
-                select: {
-                  name: true,
-                  participantLimit: true,
-                  bookings: true,
-                  venue: true,
-                  date: true
-                },
-                where: {
-                  name: {
-                    contains: query
-                  }
-                }
-              });
-              console.log(result);
-            } catch (e) {
-              console.log(e);
-            }
-            break;
-          case 'bylocation':
-            result =
-              await prisma.$queryResult`SELECT name, participant_limit, bookings, venue, date FROM Event WHERE location LIKE '%${query}%';`;
-          default:
-            // Provided with wrong or empty filter
-            return fail(400, {
-              success: fail,
-              result: []
-            });
-        }
-      } catch (e) {
-        // Error with prisma client
-        result = [];
-        return fail(500, {
-          success: false,
-          result
-        });
-      }
-      return {
-        success: true,
-        result
-      };
-    } else {
-      // When user provides no query then all the records are sent
-      try {
-        result = await prisma.event.findMany({
-          select: {
-            name: true,
-            participantLimit: true,
-            bookings: true,
-            venue: true,
-            date: true
-          }
-        });
+	default: async ({ request }) => {
+		const submitData = await request.formData();
+		const filter = submitData.get('filter');
+		const query = submitData.get('query');
+		let result: unknown;
+		console.log(query);
+		if (query) {
+			// When user provides a query and a filter
+			switch (filter) {
+				case 'bydate':
+					result =
+						await prisma.$queryRaw`SELECT name, participant_limit, bookings, venue, date FROM Event WHERE date = '${query}';`;
+					break;
+				case 'byname':
+					try {
+						result = await prisma.event.findMany({
+							select: {
+								name: true,
+								participantLimit: true,
+								bookings: true,
+								venue: true,
+								date: true
+							},
+							where: {
+								name: {
+									contains: query
+								}
+							}
+						});
+						console.log(result);
+					} catch (e) {
+						console.log(e);
+						return fail(500, {
+							success: false,
+							result: []
+						});
+					}
+					break;
+				case 'bylocation':
+					try {
+						result = await prisma.event.findMany({
+							select: {
+								name: true,
+								participantLimit: true,
+								bookings: true,
+								venue: true,
+								date: true
+							},
+							where: {
+								location: {
+									contains: query
+								}
+							}
+						});
+						console.log(result);
+					} catch (e) {
+						console.log(e);
+						return fail(500, {
+							success: false,
+							result: []
+						});
+					}
+					break;
+				default:
+					// Provided with wrong or empty filter
+					return fail(400, {
+						success: fail,
+						result: []
+					});
+			}
+			return {
+				success: true,
+				result
+			};
+		} else {
+			// When user provides no query then all the records are sent
+			try {
+				result = await prisma.event.findMany({
+					select: {
+						name: true,
+						participantLimit: true,
+						bookings: true,
+						venue: true,
+						date: true
+					}
+				});
 
-        console.log(result);
-      } catch (e) {
-        // In case prisma throws an error
-        console.log(e);
-        return fail(500, {
-          success: false,
-          result: []
-        });
-      }
-    }
-    return result;
-  }
+				console.log(result);
+			} catch (e) {
+				// In case prisma throws an error
+				console.log(e);
+				return fail(500, {
+					success: false,
+					result: []
+				});
+			}
+		}
+		return result;
+	}
 } satisfies Actions;
