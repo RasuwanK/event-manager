648771a3028959646a5ad2011a04d12a77b9aba8
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-14 20:33:07 +0530

    Added a search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
 	import Button from "$lib/Button.svelte";
-
+	import type { ActionData } from "./$types";
+  import {enhance} from '$app/forms';
+  export let form: ActionData
+  console.log(form?.result);
 </script>
 <div id="home-page">
-  <form id="searchbox" method="post">
+  <form id="searchbox" method="post" use:enhance>
     <select name="filter">
       <option value="bydate">By Date</option>
       <option value="byname">By name</option>
       <option value="bylocation">By location</option>
     </select>
-    <input type="search" />
+    <input name="query" type="search" />
     <Button type="submit">Search</Button>
   </form>
   <ul id="eventlist">
