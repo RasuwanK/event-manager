891f750b1a879017444ec15c45c417a88ccdeb39
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-15 00:48:04 +0530

    Improved form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
-	import Button from "$lib/Button.svelte";
-	import type { ActionData } from "./$types";
-  import {enhance} from '$app/forms';
-  export let form: ActionData
-  console.log(form?.result);
+	import Button from '$lib/Button.svelte';
+	import type { ActionData } from './$types';
+	import { enhance } from '$app/forms';
+	export let form: ActionData;
+	let filter = form?.filter ? form?.filter : 'byname';
 </script>
+
 <div id="home-page">
-  <form id="searchbox" method="post" use:enhance>
-    <select name="filter">
-      <option value="bydate">By Date</option>
-      <option value="byname">By name</option>
-      <option value="bylocation">By location</option>
-    </select>
-    <input name="query" type="search" />
-    <Button type="submit">Search</Button>
-  </form>
-  <ul id="eventlist">
-    
-  </ul>
+	<article>
+		{#if form?.success}
+			<p>{JSON.stringify(form?.result)}</p>
+		{/if}
+	</article>
+	<form id="searchbox" method="post" use:enhance>
+		<select bind:value={filter} name="filter">
+			<option value="bydate">By Date</option>
+			<option value="byname">By name</option>
+			<option value="bylocation">By location</option>
+		</select>
+		{#if filter === 'bydate'}
+			<input name="query" type="date" value={form?.query ? form?.query : ""} />
+		{:else}
+			<input name="query" type="search" value={form?.query ? form?.query : ""} />
+		{/if}
+		<Button type="submit">Search</Button>
+	</form>
+	<ul id="eventlist" />
 </div>
