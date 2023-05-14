94775ac076ed4e158a6d0dea88fd1ab32fdb78d0
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-14 23:43:58 +0530

    Prisma client initialization fixes

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { NODE_ENV } from '$env/static/private';
 
-const prisma =
-	global.prisma ||
-	new PrismaClient({
+const prisma = new PrismaClient({
 		log: ['query', 'error', 'info', 'warn']
 	});
-
-if (NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
