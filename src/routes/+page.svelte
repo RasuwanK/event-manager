1cdcdce50dbd96ee0145773bf1ff8035fa75b787
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-15 20:29:45 +0530

    Integrating search component with the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,13 @@
 <script lang="ts">
 	import type { ActionData } from './$types';
-	import { enhance } from '$app/forms';
-	import searchSVG from '$lib/svgs/search.svg';
 	import { page } from '$app/stores';
+	import SearchBox from '$lib/SearchBox.svelte';
 
 	export let form: ActionData;
-	let filter = form?.filter ? form?.filter : 'byname';
 </script>
 
 <div id="home-page" class="flex flex-col w-full">
-	<form
-		id="searchbox"
-		method="post"
-		use:enhance
-		class="self-center flex flex-row w-[50em] justify-stretch h-20"
-	>
-		<select bind:value={filter} name="filter" class="bg-[#222222] text-white rounded-l-full p-4">
-			<option value="byname">By name </option>
-			<option value="bydate">By Date</option>
-			<option value="bylocation">By location</option>
-		</select>
-		<div
-			id="searchbox"
-			class="rounded-r-full border-2 border-[#222222] flex flex-row w-full grow items-stretch outline-none"
-		>
-			<input
-				name="query"
-				placeholder="Search any event you want"
-				type={filter === 'bydate' ? 'date' : 'text'}
-				value={form?.query ? form?.query : ''}
-				class="outline-none grow px-3"
-			/>
-			<button
-				type="submit"
-				class="drop-shadow-lg rounded-full m-2 w-16 h-16 flex flex-col items-center justify-center text-white bg-[#222222] self-center"
-			>
-				<img src={searchSVG} width="40" height="40" alt="search" />
-			</button>
-		</div>
-	</form>
+  <SearchBox prevQuery={form?.query} prevFilter={form?.filter} />
 	<article id="events">
 		{#if $page.status === 400}
 			<p>Invalid form submission</p>
