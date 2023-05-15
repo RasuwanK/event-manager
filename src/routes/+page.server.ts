6496e3b623da44013705506681cd3e1cda2da5a5
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-15 20:26:25 +0530

    Returning proper values with proper datatypes

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -16,97 +16,7 @@ export const actions = {
 				result: []
 			});
 
-		if (query) {
-			// When user provides a query and a filter
-			switch (filter) {
-				case 'bydate':
-					try {
-						result = await prisma.event.findMany({
-							select: {
-								name: true,
-								participantLimit: true,
-								bookings: true,
-								venue: true,
-								date: true
-							},
-							where: {
-								date: {
-									gte: new Date(query as string).toISOString()
-								}
-							}
-						});
-					} catch (e) {
-						console.log(e);
-						return fail(500, {
-							success: false,
-							filter,
-							query,
-							result: []
-						});
-					}
-					break;
-				case 'byname':
-					try {
-						result = await prisma.event.findMany({
-							select: {
-								name: true,
-								participantLimit: true,
-								bookings: true,
-								venue: true,
-								date: true
-							},
-							where: {
-								name: {
-									contains: query as string
-								}
-							}
-						});
-					} catch (e) {
-						console.log(e);
-						return fail(500, {
-							success: false,
-							filter,
-							query,
-							result: []
-						});
-					}
-					break;
-				case 'bylocation':
-					try {
-						result = await prisma.event.findMany({
-							select: {
-								name: true,
-								participantLimit: true,
-								bookings: true,
-								venue: true,
-								date: true
-							},
-							where: {
-								venue: {
-									contains: query as string
-								}
-							}
-						});
-					} catch (e) {
-						console.log(e);
-						return fail(500, {
-							success: false,
-							filter,
-							query,
-							result: []
-						});
-					}
-					break;
-				default:
-					// Provided with wrong filter
-					return fail(400, {
-						success: false,
-						filter,
-						query,
-						result: []
-					});
-			}
-		} else {
+		if (!query) {
 			// When user provides no query then all the records are sent
 			try {
 				result = await prisma.event.findMany({
@@ -120,6 +30,12 @@ export const actions = {
 				});
 
 				console.log(result);
+				return {
+					success: true,
+					filter: filter.toString(),
+					query: filter.toString(),
+					result: []
+				};
 			} catch (e) {
 				// In case prisma throws an error
 				console.log(e);
@@ -132,6 +48,96 @@ export const actions = {
 			}
 		}
 
-		return { success: true, result, filter, query };
+		// When user provides a query and a filter
+		switch (filter) {
+			case 'bydate':
+				try {
+					result = await prisma.event.findMany({
+						select: {
+							name: true,
+							participantLimit: true,
+							bookings: true,
+							venue: true,
+							date: true
+						},
+						where: {
+							date: {
+								gte: new Date(query as string).toISOString()
+							}
+						}
+					});
+				} catch (e) {
+					console.log(e);
+					return fail(500, {
+						success: false,
+						filter,
+						query,
+						result: []
+					});
+				}
+				break;
+			case 'byname':
+				try {
+					result = await prisma.event.findMany({
+						select: {
+							name: true,
+							participantLimit: true,
+							bookings: true,
+							venue: true,
+							date: true
+						},
+						where: {
+							name: {
+								contains: query as string
+							}
+						}
+					});
+				} catch (e) {
+					console.log(e);
+					return fail(500, {
+						success: false,
+						filter,
+						query,
+						result: []
+					});
+				}
+				break;
+			case 'bylocation':
+				try {
+					result = await prisma.event.findMany({
+						select: {
+							name: true,
+							participantLimit: true,
+							bookings: true,
+							venue: true,
+							date: true
+						},
+						where: {
+							venue: {
+								contains: query as string
+							}
+						}
+					});
+				} catch (e) {
+					console.log(e);
+					return fail(500, {
+						success: false,
+						filter,
+						query,
+						result: []
+					});
+				}
+				break;
+			default:
+				// Provided with wrong filter
+				return fail(400, {
+					success: false,
+					filter,
+					query,
+					result: []
+				});
+		}
+
+		return { success: true, result, filter: filter.toString(), query: query.toString() };
 	}
 } satisfies Actions;
