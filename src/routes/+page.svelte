e0dc7886a1fd902d3c5a0c2291dead630c5bf0aa
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-15 13:07:30 +0530

    Improved appearance of the searchbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-	import Button from '$lib/Button.svelte';
 	import type { ActionData } from './$types';
 	import { enhance } from '$app/forms';
+
 	export let form: ActionData;
 	let filter = form?.filter ? form?.filter : 'byname';
 </script>
 
-<div id="home-page">
+<div id="home-page" class="flex flex-col w-full">
 	<article>
 		{#if form?.success}
 			<p>{JSON.stringify(form?.result)}</p>
 		{/if}
 	</article>
-	<form id="searchbox" method="post" use:enhance>
-		<select bind:value={filter} name="filter">
-			<option value="bydate">By Date</option>
+	<form id="searchbox" method="post" use:enhance class="self-center flex flex-row w-[80%] justify-stretch h-20">
+		<select bind:value={filter} name="filter" class="bg-[#222222] text-white rounded-l-full p-4">
 			<option value="byname">By name</option>
+			<option value="bydate">By Date</option>
 			<option value="bylocation">By location</option>
 		</select>
-		{#if filter === 'bydate'}
-			<input name="query" type="date" value={form?.query ? form?.query : ""} />
-		{:else}
-			<input name="query" type="search" value={form?.query ? form?.query : ""} />
-		{/if}
-		<Button type="submit">Search</Button>
+		<div id="searchbox" class="rounded-r-full border-2 border-[#222222] flex flex-row w-full grow items-stretch outline-none">
+			<input
+				name="query"
+				type={filter === 'bydate' ? 'date' : 'text'}
+				value={form?.query ? form?.query : ''}
+        class="outline-none grow"
+			/>
+			<button type="submit" class="drop-shadow-lg rounded-full m-2 w-16 h-16 text-white bg-[#222222] self-center">Search</button>
+		</div>
 	</form>
 	<ul id="eventlist" />
 </div>
