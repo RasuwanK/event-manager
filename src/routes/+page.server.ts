f9082f0164316e02e47de11f774ad7df09674f06
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-16 18:54:55 +0530

    Fixed server response issues to the client

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -33,7 +33,7 @@ export const actions = {
 				return {
 					success: true,
 					filter: filter.toString(),
-					query: filter.toString(),
+					query: '',
 					result: []
 				};
 			} catch (e) {
