23e3a432e5d566f0f218c9072801727838beaffc
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-19 13:53:01 +0530

    Link look like a button

---
+++ src/lib/DisplayCard/BottomLink.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let id: string;
+</script>
+<a
+	href="/book/{id}"
+	class="bg-[#222222] text-white w-full h-full flex flex-col items-center justify-center rounded-b-lg"
+	><slot /></a
+>
