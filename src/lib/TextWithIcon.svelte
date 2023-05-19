91778e82807d778aa014ec9ac066ab3a379fbbd5
Rasuwan Kalhara <kalharaweragala@gmail.com>
2023-05-19 13:49:32 +0530

    Component to display text with icons

---
+++ src/lib/TextWithIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let iconPath: string;
+	export let text: string;
+	export let iconDimensions: [number, number];
+	export let iconAlt: string;
+</script>
+
+<div id="info" class="flex flex-row items-center gap-1 my-1">
+	<span id="icon"
+		><img src={iconPath} alt={iconAlt} width={iconDimensions[0]} height={iconDimensions[1]} /></span
+	>
+	<span id="text" class="text-sm">{text}</span>
+</div>
